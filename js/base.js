dc4fb0f6c5b0072cee4ab6468f685f3570d13d90
¨julievera7¨ <julie@julie.168.1.25>
2024-05-31 17:22:23 -0300

    forma

--- js/base.js
+++ js/base.js
@@ -14,15 +14,25 @@ form.addEventListener("submit", (e)=> {
 const isValidEmail = email => {
     const correo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
     return correo.test(String(email).toLowerCase());
+
 }
 
 if (typeof form !== "undefined") {
  
 }
 
+const API_SERVER = 'https://openlibrary.org';
+const options = {
+    method: 'GET', // Método de la petición (GET)
+    headers: {
+        accept: 'application/json', // Tipo de respuesta esperada (JSON)
+        
+    }
+};
+
 function consultar(){
-  document.getElementById('bookmatch').innerHTML="";
-  fetch("https://openlibrary.org/search.json?q="+document.getElementById("buscar").value+"&fields=*,availability&limit=1").then(a=>a.json()).then(response =>{
+  document.getElementById(".bookmatch").innerHTML="";
+  fetch("${API_SERVER}/search.json?q="+document.getElementById("buscar").value).then(a=>a.json()).then(response =>{
     for(var i=0; i<3; i++){
       document.getElementById("bookmatch").innerHTML+="<h3>"+response.docs[i].title+"</h3>"+response.docs[i].author_name[0]+"<br><img src='https://covers.openlibrary.org/a/olid/"+response.docs[i].key[0]+"-M.jpg'><br>";
     }
