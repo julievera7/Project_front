44c5d482e93bdf06bf5a45979b08638168a10a9e
¨julievera7¨ <julie@julie.168.1.25>
2024-06-10 22:13:23 -0300

    Solved_FueraFormyclases

--- js/base.js
+++ js/base.js
@@ -20,8 +20,8 @@ const isValidEmail = email => {
 if (typeof form !== "undefined") {
  
 }
-/*
-const API_SERVER = "https://openlibrary.org/search.json";
+
+const API_SERVER = "http://openlibrary.org/search.json";
 const headers = new Headers({
   "User-Agent": "projectfront/1.0 (julievera7@gmail.com)"
 });
@@ -32,14 +32,14 @@ const options = {
         
     }
 };
-*/
+
 function getbooks(){
   document.getElementById("books").innerHTML="";
-  fetch("https://openlibrary.org/search.json?q="+document.getElementById("booksearch").value)
+  fetch("http://openlibrary.org/search.json?q="+document.getElementById("booksearch").value)
   .then(a=>a.json())
   .then(Response=>{
-      for(var i=0; i<10; i++) {
-          document.getElementById("books").innerHTML+="<h2>"+Response.docs[i].title+"</h2>"+Response.docs[i].author_name[0]+"<br><img src='https://covers.openlibrary.org/b/isbn/"+Response.docs[i].isbn[0]+"-M.jpg'><br>";
+      for(var i=0; i<1; i++) {
+          document.getElementById("books").innerHTML+="<h2>"+Response.docs[i].title+"</h2>"+Response.docs[i].author_name[0]+"<br><img src='http://covers.openlibrary.org/b/isbn/"+Response.docs[i].isbn[0]+"-M.jpg'><br>";
 
       }
   });
