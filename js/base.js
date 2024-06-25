9f6813d65b85c2f40233dfa1800deabb679563b7
¨julievera7¨ <julie@julie.168.1.25>
2024-06-25 20:44:14 -0300

    version 2.0

--- js/base.js
+++ js/base.js
@@ -1,26 +1,3 @@
-const form = document.getElementById("formmain");
-const email = document.getElementById("email");
-
-form.addEventListener("submit", (e)=> {
-    if(form.checkValidity()===false) {
-      e.preventDefault();
-      e.stopPropagation();
-      form.classList.add("was-validated");
-      validateInput();
-      return false;
-    }
-  });
-
-const isValidEmail = email => {
-    const correo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    return correo.test(String(email).toLowerCase());
-
-}
-
-if (typeof form !== "undefined") {
- 
-}
-
 const API_SERVER = "http://openlibrary.org/search.json";
 const headers = new Headers({
   "User-Agent": "projectfront/1.0 (julievera7@gmail.com)"
@@ -39,23 +16,30 @@ function getbooks(){
   .then(a=>a.json())
   .then(Response=>{
       for(var i=0; i<6; i++) {
-          document.getElementById("books").innerHTML+="<div class='col'>"+"<div class='card'>"+"<img class='card-img-top' src='http://covers.openlibrary.org/b/isbn/"+Response.docs[i].isbn[0]+"-M.jpg'>"+"<div class='card-body'>"+"<h5 class='card-title'>"+Response.docs[i].title+"</h5>"+"<p class='card-text'>"+Response.docs[i].author_name[0]+"</p>"+"</div>"+"</div>"+"</div>";
+          document.getElementById("books").innerHTML+="<br><div class='col'>"+"<div class='card'>"+"<img class='card-img-top' src='http://covers.openlibrary.org/b/isbn/"+Response.docs[i].isbn[0]+"-M.jpg'>"+"<div class='card-body'>"+"<h5 class='card-title'>"+Response.docs[i].title+"</h5>"+"<p class='card-text'>"+Response.docs[i].author_name[0]+"</p>"+"</div>"+"</div>"+"</div>";
 
       }
   });
 }
 
-      
-        
-        
-         
-          
-       
-       
-/*
-for(var i=0; i<3; i++) {
-          document.getElementById("books").innerHTML+="<img class='card' sizes='width: 5%' src='http://covers.openlibrary.org/b/isbn/"+Response.docs[i].isbn[0]+"-M.jpg'>"+Response.docs[i].title;
-          */
-function reiniciar(){
-  fetch("https://julievera7.github.io/Project_front/index.html")
-}
+
+const addForm = document.getElementById("login");
+
+addForm.addEventListener("submit", (e)=> {
+  if(addForm.checkValidity() ===false){
+    e.preventDefault();
+    e.stopPropagation();
+    addForm.classList.add('was-validated');
+    return false
+  }
+})
+
+const addRegistro = document.getElementById("registro");
+addRegistro.addEventListener("submit", (e)=> {
+  if(addRegistro.checkValidity() ===false){
+    e.preventDefault();
+    e.stopPropagation();
+    addRegistro.classList.add('was-validated');
+    return false
+  }
+})
